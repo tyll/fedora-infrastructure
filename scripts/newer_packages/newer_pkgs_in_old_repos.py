90e6dc025996791e2c3283ff9560e869c4cdb070
Mike McGrath <mmcgrath@redhat.com>
2010-08-17 14:44:07 -0500

    Adding infra notes so I don't forget them

--- scripts/newer_packages/newer_pkgs_in_old_repos.py
+++ scripts/newer_packages/newer_pkgs_in_old_repos.py
@@ -15,22 +15,23 @@ my.preconf.debuglevel=0
 my.arch.archlist.append('src')
 my.setCacheDir()
 my.repos.disableRepo('*')
-my.add_enable_repo('f13',
-   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/releases/13/Everything/source/'])
-#my.add_enable_repo('f13-updates-testing',
-#   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/updates/testing/13/SRPMS/'])   
-my.add_enable_repo('f13-updates',
-   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/updates/13/SRPMS/',
-            'http://download.fedora.redhat.com/pub/fedora/linux/updates/testing/13/SRPMS/'])
-my.add_enable_repo('f12', 
-   baseurls=['http://fedora.mirrors.tds.net/pub/fedora/releases/12/Everything/source/SRPMS/'])
-my.add_enable_repo('f12-updates', 
-   baseurls=['http://fedora.mirrors.tds.net/pub/fedora/updates/12/SRPMS/'])
+my.add_enable_repo('f14',
+   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/development/14/source/SRPMS/'])
+my.add_enable_repo('f14-updates',
+   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/updates/testing/14/SRPMS/'])   
+#my.add_enable_repo('f14-updates',
+#   baseurls=['http://download.fedora.redhat.com/pub/fedora/linux/updates/14/SRPMS/',
+#            'http://download.fedora.redhat.com/pub/fedora/linux/updates/testing/14/SRPMS/'])
+my.add_enable_repo('f13', 
+   baseurls=['http://fedora.mirrors.tds.net/pub/fedora/releases/13/Everything/source/SRPMS/'])
+my.add_enable_repo('f13-updates', 
+   baseurls=['http://fedora.mirrors.tds.net/pub/fedora/updates/13/SRPMS/',
+              'http://download.fedora.redhat.com/pub/fedora/linux/updates/testing/13/SRPMS/'])
 
 len(my.pkgSack) # just to frob the sack
 
-f13repo = my.repos.findRepos('f13')[0]
-f13updates = my.repos.findRepos('f13-updates')[0]
+f14repo = my.repos.findRepos('f14')[0]
+f14updates = my.repos.findRepos('f14-updates')[0]
 
 def whoowns(package):
     """<package>
@@ -58,25 +59,25 @@ def whoowns(package):
 
 owners = []
 for pkg in sorted(my.pkgSack.returnNewestByNameArch()):
-    if not pkg.repoid.startswith('f13'):
-        f13pkgs = []
-        f13upkgs = []
-        f13all = []
+    if not pkg.repoid.startswith('f14'):
+        f14pkgs = []
+        f14upkgs = []
+        f14all = []
         try:
-            f13pkgs = f13repo.sack.returnNewestByNameArch((pkg.name, pkg.arch))
-            f13upkgs = f13updates.sack.returnNewestByNameArch((pkg.name, pkg.arch))
+            f14pkgs = f14repo.sack.returnNewestByNameArch((pkg.name, pkg.arch))
+            f14upkgs = f14updates.sack.returnNewestByNameArch((pkg.name, pkg.arch))
         except yum.Errors.PackageSackError, e:
             pass
-        f13all.extend(f13upkgs)
-        f13all.extend(f13pkgs)
-        if f13all:
-            f13all.sort()
-            f13all.reverse()
-            if f13all[0].EVR != pkg.EVR:
+        f14all.extend(f14upkgs)
+        f14all.extend(f14pkgs)
+        if f14all:
+            f14all.sort()
+            f14all.reverse()
+            if f14all[0].EVR != pkg.EVR:
                 owner = whoowns(pkg.name)
                 owners.append(owner)
-                print 'greater for f12: %s (%s)' % (pkg.name, owner)
-                print ' f12 = %s' % pkg
-                print ' f13 = %s' % f13all[0]
+                print 'greater for f13: %s (%s)' % (pkg.name, owner)
+                print ' f13 = %s' % pkg
+                print ' f14 = %s' % f14all[0]
 
 print '%s@fedoraproject.org' % '@fedoraproject.org,'.join(sorted(set(owners)))
