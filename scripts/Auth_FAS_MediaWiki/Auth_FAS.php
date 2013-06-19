c34d9972fe708307f8faaf9c0f97b42955e0e438
Sijis Aviles <sijis@fedoraproject.org>
2013-06-18 21:36:55 -0500

    updating mw fas plugin with mostly working code to allow users to login to with username or username@fp.o

--- scripts/Auth_FAS_MediaWiki/Auth_FAS.php
+++ scripts/Auth_FAS_MediaWiki/Auth_FAS.php
@@ -1,12 +1,24 @@
 <?php
 require_once('AuthPlugin.php');
 class Auth_FAS extends AuthPlugin {
-    function authenticate($username, $password) {
+
+    var $fas_username;
+
+    function setFasUsername($user) {
+        $this->fas_username = $user;
+    }
+
+    function getFasUsername() {
+        return $this->fas_username;
+    }
+
+    function authenticate(&$username, $password) {
+        
         if ( ucfirst(strtolower($username)) != ucfirst($username) ) {
             return false;
         }
 
-        $username = strtolower( $username);
+        $username = strtolower($username);
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, 'https://admin.fedoraproject.org/accounts/home');
@@ -35,6 +47,9 @@ class Auth_FAS extends AuthPlugin {
         }
 
         $groups = $response['memberships'];
+        // let's make sure the username is consistent
+        $this->setFasUsername(ucfirst(strtolower($response['person']['username'])));
+        $username = $this->getFasUsername();
 
         for ($i = 0, $cnt = count($groups); $i < $cnt; $i++) {
             if ($groups[$i]["name"] == 'cla_done' && $response['person']['status'] == 'active') {
@@ -47,68 +62,103 @@ class Auth_FAS extends AuthPlugin {
     }
 
     function userExists( $username ) {
-        if ( ucfirst(strtolower($username)) != ucfirst($username) ) {
+        error_log("FAS [userExists]: $username, " . $this->getFasUsername(), 0);
+
+        if (ucfirst(strtolower($username)) != $this->getFasUsername()) {
+            error_log("FAS [userExists]: returned false", 0);
             return false;
-        }
+        }                         
+        error_log("FAS [userExists]: returned true", 0);
         return true;
     }
 
     function modifyUITemplate(&$template) {
+        error_log("FAS [modifyUITemplate]: " . $this->getFasUsername(), 0);
         $template->set('create', false);
         $template->set('useemail', false);
         $template->set('usedomain', false);
     }
 
     function updateUser( &$user ){
+        //error_log("FAS [updateUser]: " . $user->getName() . ", " . $this->getFasUsername(), 0);
+        $user->setName($this->getFasUsername());
         $user->mEmail = strtolower($user->getName())."@fedoraproject.org";
+        //error_log("FAS [updateUser]: " . $user->getName() . ", " . $this->getFasUsername(), 0);
         return true;
     }
 
     function autoCreate() {
+        //error_log("FAS [autoCreate]: ", 0);
         return true;
     }
 
     function setPassword($password) {
+        //error_log("FAS [setPassword]: $password", 0);
         return false;
     }
 
     function setDomain( $domain ) {
+        //error_log("FAS [setDomain]: $domain", 0);
         $this->domain = $domain;
     }
 
     function validDomain( $domain ) {
+        //error_log("FAS [validDomain]: $domain", 0);
         return true;
     }
 
     function updateExternalDB($user) {
+        //error_log("FAS [updateExternalDB]: $user", 0);
         return true;
     }
 
     function canCreateAccounts() {
+        //error_log("FAS [canCreateAccounts]:", 0);
         return false;
     }
 
     function addUser($user, $password) {
+        //error_log("FAS [addUser]: $user, $password", 0);
         return true;
     }
 
     function strict() {
+        //error_log("FAS [strict]:", 0);
         return true;
     }
 
     function strictUserAuth( $username ) {
+        //error_log("FAS [strictUserAuth]: $username", 0);
         return true;
     }
 
     function allowPasswordChange() {
+        //error_log("FAS [allowPasswordChange]:" . $this->getFasUsername(), 0);
         return false;
     }
 
+    function getCanonicalName( $username ) {
+        //error_log("FAS [getCanonicalName]: " . $username, 0);
+
+        $username = str_replace('@fedoraproject.org', '', $username);
+
+        //error_log("FAS [getCanonicalName]: returning... " . $username, 0);
+        return $username;
+    }
+
+    function getUserInstance( &$user ) {
+        //error_log("FAS [getUserInstance]: " . print_r($user), 0);
+        return new AuthPluginUser( $user );
+    }
+
     function initUser(&$user) {
+        //error_log("FAS [initUser]: " . $user->getName() . ", " . $this->getFasUsername(), 0);
+        $user->setName($this->getFasUsername());
         $user->mEmail = strtolower($user->getName())."@fedoraproject.org";
         $user->mEmailAuthenticated = wfTimestampNow();
         $user->setToken();
         $user->saveSettings();
+        //error_log("FAS [initUser]: " . $user->getName() . ", " . $this->getFasUsername(), 0);
         return true;
     }
 }
